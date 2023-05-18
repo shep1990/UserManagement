7671b533da86ce2fc23a83b0fd707c9b6e0e7d33
Richard Shepherd <richardjohn90@hotmail.co.uk>
2023-05-18 07:15:27 +0100

    Add project files.

---
+++ UserManagement.Data/IDataContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UserManagement.Models;
+
+namespace UserManagement.Data;
+
+public interface IDataContext
+{
+    /// <summary>
+    /// Get a list of items
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <returns></returns>
+    IQueryable<TEntity> GetAll<TEntity>() where TEntity : class;
+
+    /// <summary>
+    /// Create a new item
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    void Create<TEntity>(TEntity entity) where TEntity : class;
+
+    /// <summary>
+    /// Uodate an existing item matching the ID
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    void Update<TEntity>(TEntity entity) where TEntity : class;
+
+    void Delete<TEntity>(TEntity entity) where TEntity : class;
+
+    IQueryable<TEntity> GetActiveUsers<TEntity>() where TEntity : class, IUser;
+}
---
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UserManagement.Services.Domain.Interfaces;
+using UserManagement.Web.Models.Users;
+
+namespace UserManagement.WebMS.Controllers;
+
+[Route("users")]
+public class UsersController : Controller
+{
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService) => _userService = userService;
+
+    [HttpGet("AllUsers")]
+    public ViewResult List()
+    {
+        var items = _userService.GetAll().Select(p => new UserListItemViewModel
+        {
+            Id = p.Id,
+            Forename = p.Forename,
+            Surname = p.Surname,
+            Email = p.Email,
+            IsActive = p.IsActive
+        });
+
+        var model = new UserListViewModel
+        {
+            Items = items.ToList()
+        };
+
+        return View(model);
+    }
+
+    [HttpGet("ActiveUsers")]
+    public ViewResult GetActiveUsers()
+    {
+        var items = _userService.GetActiveUsers().Select(p => new UserListItemViewModel
+        {
+            Id = p.Id,
+            Forename = p.Forename,
+            Surname = p.Surname,
+            Email = p.Email,
+            IsActive = p.IsActive
+        });
+
+        var model = new UserListViewModel
+        {
+            Items = items.ToList()
+        };
+
+        return View("List",model);
+    }
+}
